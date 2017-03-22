ba886435d7c587e1859a69f2c323f43b94680536
Yo <Yo@192.168.71.128>
2017-03-22 14:51:26 +0100

    merge

--- github/src/github/prueba.java
+++ github/src/github/prueba.java
@@ -4,6 +4,7 @@ public class prueba {
 	int data[]={-1,0,4,42,84};
 	
 	public void display(){
+		
 		System.out.println("Data: ");
 		for (int i:data){
 			System.out.println("Next value: " + data[i]);
