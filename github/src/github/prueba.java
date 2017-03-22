ca1f7d4c8680fdb08cb0495a8f1bb8c5f873e2b7
Yo <Yo@192.168.71.128>
2017-03-22 14:07:26 +0100

    cosas del github

---
+++ github/src/github/prueba.java
@@ -0,0 +1,12 @@
+package github;
+
+public class prueba {
+	int data[]={0,4,42};
+	
+	public void display(){
+		System.out.println("Data: ");
+		for (int i=0; i<data.length; i++){
+			System.out.println("Next value: " + data[i]);
+		}
+	}
+}
